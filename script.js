7124b7386159f48a5143b886b22f129c83df2799
bandeiraos <pedrocargninbandeira@gmail.com>
2024-01-25 11:53:53 -0300

    added fetch countries

--- script.js
+++ script.js
@@ -5,27 +5,24 @@ const CARD_STATUS = {
     REVEALED: 'revealed'
 };
 
-const CARDS = [
-    { name: 'A', img: 'AA' },
-    { name: 'B', img: 'BB' },
-];
-
 const divWrapper = document.querySelector('.wrapper');
 const divScore = document.querySelector('.js-score');
 
+let _cards = [];
 let _selectedCard = null;
 let _score = 0;
+let _blockClick = false;
 
 function drawScore() {
-    divScore.textContent = `${_score} / ${CARDS.length}`;
+    divScore.textContent = `${_score} / ${_cards.length}`;
 }
 
 function setScore() {
     _score++;
     drawScore();
 
-    if (_score === CARDS.length) {
-        alert('win');
+    if (_score === _cards.length) {
+        console.log('win');
     }
 }
 
@@ -57,36 +54,43 @@ function handleCardClick(divCard) {
     const selecterdAnswer = _selectedCard.getAttribute("data-answer");
     const currentCardAnswer = divCard.getAttribute("data-answer");
 
-    if (selecterdAnswer === currentCardAnswer) {
-        correctCard(divCard);
-        setScore();
-    } else {
-        hideCard(divCard);
-    }
+    _blockClick = true;
 
-    _selectedCard = null;
+
+    setTimeout(() => {
+        if (selecterdAnswer === currentCardAnswer) {
+            correctCard(divCard);
+            setScore();
+        } else {
+            hideCard(divCard);
+        }
+        _blockClick = false;
+        _selectedCard = null;
+    }, 1000);
+
 }
 
 function createCards() {
-    CARDS.forEach(card => {
+    _cards.forEach(card => {
         let divCardName = document.createElement('div'),
-            divCardAnswer = document.createElement('div'),
+            divCardImg = document.createElement('div'),
             divCardNameContent = document.createElement('div'),
-            divCardAnswerContent = document.createElement('div');
+            divCardImgContent = document.createElement('div');
 
         divCardName.className = 'card hidden';
-        divCardName.setAttribute('data-answer', card.img);
+        divCardName.setAttribute('data-answer', card.flag);
         divCardNameContent.textContent = card.name;
 
-        divCardAnswer.className = 'card hidden';
-        divCardAnswer.setAttribute('data-answer', card.img);
-        divCardAnswerContent.textContent = card.img;
+        divCardImg.className = 'card hidden';
+        divCardImg.setAttribute('data-answer', card.flag);
 
+        divCardImgContent.className = 'flag';
+        divCardImgContent.style.backgroundImage = `url(${card.flag})`;
 
         divCardName.append(divCardNameContent);
-        divCardAnswer.append(divCardAnswerContent);
+        divCardImg.append(divCardImgContent);
         divWrapper.append(divCardName);
-        divWrapper.append(divCardAnswer);
+        divWrapper.append(divCardImg);
     });
 }
 
@@ -96,17 +100,44 @@ function delegateEvt() {
         if (targetClasslist.contains(CARD_STATUS.REVEALED)) {
             return null;
         } else if (targetClasslist.contains(CARD_STATUS.HIDDEN)) {
+            if (_blockClick) return;
+
             revealCard(e.target);
             setTimeout(() => handleCardClick(e.target), 500);
         }
     });
 }
 
-function start() {
-    delegateEvt();
-    drawScore();
+async function fetchCountries() {
+    const response = await fetch("./data.json"),
+        data = await response.json(),
+        chosenNumbers = [];
+
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 249);
+    }
+
+    while (chosenNumbers.length < 6) {
+        const randomNumber = generateRandomNumber();
+        if (!chosenNumbers.includes(randomNumber)) {
+            chosenNumbers.push(randomNumber);
+        }
+    }
+
+    chosenNumbers.forEach(num => {
+        const { name, flags } = data[num];
+
+        _cards.push({ name, flag: flags.png });
+    });
+
+
     createCards();
+    drawScore();
 }
 
+function start() {
+    fetchCountries();
+    delegateEvt();
+}
 
 start();
